4e1c439636f43af88555988ab321f2a5964f7b1c
adi65 <adija@edu.hac.ac.il>
2024-03-23 18:20:12 +0200

    added signUp route

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,6 @@ router.get('/newAd', indexController.getNewAdPage); // Route for rendering new a
 router.get('/login', indexController.getLoginPage); // Route for rendering login page
 router.get('/adminPage', isAdmin, indexController.getAdminPage); // Route for rendering admin page, with authentication middleware
 router.get('/success', indexController.getSuccessPage); // Route for rendering success page
+router.get('/signUpPage', indexController.getSignUpPage); // Route for rendering success page
 
 module.exports = router;
